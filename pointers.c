410dca51b24fb71be79af7f1d037dd8e15316028
Kaiden Krenek <krenekk7916@my.sduhsd.net>
2020-01-06 13:06:49 -0800

    first commit

---
+++ pointers.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+int main()
+{
+
+  int a;
+  int* ptrtoa;
+
+  ptrtoa = &a;
+
+  a = 5;
+  printf("The value of a is %d\n", a);
+
+  *ptrtoa = 6;
+  printf("The value of a is %d\n", a);
+
+  printf("The value of ptrtoa is %d\n", ptrtoa);
+  printf("It stores the value %d\n", *ptrtoa);
+  printf("The address of a is %d\n", &a);
+
+float d = 10;
+float e =20;
+
+float* ptrtod = &d;
+
+printf("The value of d is %f  The address of d is %f\n", d, ptrtod);
+float* ptrtoe = &e;
+
+printf("The value of e is %f  The address of e is %f\n", e, ptrtoe);
+
+
+ptrtoe*
+
+
+int temp = ptrtod;
+
+ ptrtod= ptrtoe;
+
+ ptrtoe = temp;
+
+
+}
