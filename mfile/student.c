e3678b67dfd397f5b07a7214c7b4de770c5caf8d
trygamer <trygamer777@gmail.com>
2020-01-10 21:06:11 -0800

    Done

--- mfile/student.c
+++ mfile/student.c
@@ -3,3 +3,63 @@
 #include <string.h>
 
 #include "student.h"
+
+struct Student 
+
+
+
+{
+
+
+
+int null;
+
+
+
+  int age;
+
+
+
+ char firstName[256];
+
+
+
+  int studentID;
+
+
+
+  char lastName[256];
+
+
+
+};
+
+
+
+
+
+
+
+void printStudent(struct Student* student)
+
+
+
+{
+
+
+
+  printf("First Name: %s \n", student->firstName);
+
+  printf("Last Name %s\n",  student->lastName);
+
+
+
+  printf("  Age: %d\n", student->age);
+
+
+
+  printf("  Student ID: %d\n", student->studentID);
+
+
+
+}
