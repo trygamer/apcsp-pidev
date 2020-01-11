bc8663fa8d3c6d06105b46e04e4ba536f3f6a3e9
trygamer <trygamer777@gmail.com>
2020-01-10 19:47:21 -0800

    Fixed

--- array.c
+++ array.c
@@ -4,24 +4,51 @@
 
 
 
+
+
+
+
+
+
 int arrayAdd(int* arr, int s, int n)
+
 {
+
   
+
   for(int i = 0; i<s+1;i++){
+
     arr[i]=i*n; 
+
     printf("function running %d\n",arr[i]);
 
+
+
   }
+
 }
 
+
+
 int main()
+
 {
+
 int arr[100];
+
   for(int i=0;i<100;i++)
+
   {
+
   arr[i]=i*i;
+
   }
-  printf("index 4: %d index 8: %d\n", arr[4],arr[8]);
+
+  printf("For first Assignment index 4: %d index 8: %d\n", arr[4],arr[8]);
+printf("this is a test for the function");
   arrayAdd(arr,50,10);
+  printf("done");
+
   return 0;
+
 }
