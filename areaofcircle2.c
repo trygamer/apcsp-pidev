21f4cf6adc97718e9b9844042198b6e43a33534f
trygamer <trygamer777@gmail.com>
2020-01-10 20:15:57 -0800

    fixed

--- areaofcircle2.c
+++ areaofcircle2.c
@@ -3,44 +3,91 @@
 
 
 
+
+
+
+
+
 /*
+
 int main()
+
 {	
+
   char input[256], name[256];
+
   int age;
+
   
+
   printf("What is your name?\n");
+
   fgets(input, 256, stdin);
+
   sscanf(input, "%s", name);
 
+
+
   printf("Hello %s. How old are you?\n", name);
+
   while (1)
+
   {
+
     fgets(input, 256, stdin);
+
     if (sscanf(input, "%d", &age) == 1) break;
+
     printf("Not a valid age - try again!\n");
+
   }
+
   
+
   printf("Hi %s, you look young for %d…\n", name, age);
+
 }
+
 */
+
 int main(int argc, char* argv[])
+
 {
-  int arg1 =1 ;
-  int arg2 = 2;
+
+  float arg1;
+
+  float arg2;
+
   int check = 2;
-  while(check==2||3)
-  {
-    if(sscanf(input, "%d",arg1){
-    check++;
-    } 
-  } 
 
 
-printf("%d  %d",argv[arg1],argv[arg2]);
-  for(int i =argv[arg1];i<argv[arg2]+1;i++)
+ 
+
+   int lower =sscanf(argv[1],"%f", &arg1);
+   int upper =sscanf(argv[2],"%f", &arg2); 
+
+if (arg2<arg1){
+float temp =arg1;
+arg1 =arg2;
+arg2 =temp;
+
+}
+   
+
+
+
+
+
+printf("%f  %f\n",arg1,arg2);
+
+  for(float i =arg1;i<arg2+1;i++)
+
   {
-    int area = i*i*3.14;
-    printf("The radius is %d and the area is %d\n", i,area); 
+
+    float area = i*i*3.14;
+
+    printf("The radius is %f and the area is %f\n", i,area); 
+
   }
+
 }
